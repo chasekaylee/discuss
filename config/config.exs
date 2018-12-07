cb489e0e940d001905ded3c7f4b73a6dbe5bcf54
Chase Lee <chaseklee990@gmail.com>
2018-12-07 11:18:07 -0800

    Implement oAuth flows for github using ueberauth

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "4f6f28582db513253b59",
+  client_secret: "ac4deaca3e66cc38c109c757c9c8b5b536f2d03f"
